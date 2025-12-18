5e867bbe382c46af2e39c616ec85b0e6951693de
Clinton Mills <clinton.mills@spartancyberservices.com>
2025-12-17 21:59:55 -0600

    feat: update version and add server entrypoint to SDK

---
+++ dist/server.js
@@ -0,0 +1,16 @@
+/**
+ * HIT SDK (server-safe entrypoint)
+ *
+ * This entrypoint intentionally does NOT export the React provider/hooks.
+ * Use this from Next.js Route Handlers / Server Components to avoid importing
+ * client-only modules like `provider.js`.
+ */
+export { HitClient, HitAPIError } from './client.js';
+export { getServiceUrl, getNamespace, getApiKey, getWebSocketUrl } from './config.js';
+export { PingPongClient, pingPong, getCounter, increment, reset, getConfig, version, subscribeCounter, } from './pingPong.js';
+export { EmailClient, email, sendEmail } from './email.js';
+export { AuthClient, auth, register, login, verifyEmail, enableTwoFactor, verifyTwoFactor, oauthUrl, oauthCallback, config as authConfig, features as authFeatures, } from './auth.js';
+export { HitEvents, events, getEventsClient, } from './events.js';
+export { parseDate, formatDate, formatDateTime, formatDateShort, formatTime, formatRelativeTime, formatISO, formatSmart, } from './date-utils.js';
+
+
